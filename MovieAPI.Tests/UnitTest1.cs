89d63c29342fc9d5250bdcbf150fdf50b2755131
= <=>
2021-12-12 16:32:34 +0100

    bracktes fix

--- MovieAPI.Tests/UnitTest1.cs
+++ MovieAPI.Tests/UnitTest1.cs
@@ -1,14 +1,11 @@
 using System;
 using Xunit;
 
-namespace MovieAPI.Tests
-{
-    public class UnitTest1
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace MovieAPI.Tests {  
+	public class UnitTest1 {
+    [Fact]
+    public void Test1() {
 
-        }
     }
+  }
 }
