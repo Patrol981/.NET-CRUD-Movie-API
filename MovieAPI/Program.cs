22125353709082051e0228c7d6143756b70ef3c8
Patryk Markowski <patrol56789@gmail.com>
2022-03-10 11:03:48 +0100

    redirect to https only in prod

--- MovieAPI/Program.cs
+++ MovieAPI/Program.cs
@@ -21,7 +21,7 @@ namespace MovieAPI {
           })
           .ConfigureWebHostDefaults(webBuilder => {
             webBuilder.UseStartup<Startup>();
-            webBuilder.UseUrls("http://*:5000");
+            // webBuilder.UseUrls("http://*:5000");
           });
   }
 }
--- MovieAPI/Startup.cs
+++ MovieAPI/Startup.cs
@@ -54,7 +54,9 @@ namespace MovieAPI {
         app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "MovieAPI v1"));
       }
 
-      app.UseHttpsRedirection();
+      if(env.IsProduction()) {
+        app.UseHttpsRedirection();
+      }
 
       app.UseCors("AllowOrigin");
 
